9f61c2ff37f30e6cab24642c0d47d020ac30a960
Rils App <rilsapp.dev@gmail.com>
2023-04-10 12:15:57 +0800

    api openai

--- ai/php/api.php
+++ ai/php/api.php
@@ -2,7 +2,7 @@
 ini_set("display_errors", 0);
 
 // Set up API key, URL, and model
-$API_KEY = '';
+$API_KEY = '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';
 $url = 'https://api.openai.com/v1/completions';
 $model = "text-davinci-003";
 $chat = "";
